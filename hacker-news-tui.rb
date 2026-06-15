30aef923add071a2d8ce7b79ba0f6de90fb30b1a
Daniel Fry <danf@hotmail.co.za>
2026-06-15 20:33:02 +0100

    Update hacker-news-tui to 0.1.2

--- hacker-news-tui.rb
+++ hacker-news-tui.rb
@@ -4,13 +4,13 @@
 class HackerNewsTui < Formula
   desc "Delightful terminal UI for browsing Hacker News, built with ratatui"
   homepage "https://github.com/danfry1/hacker-news-tui"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/danfry1/hacker-news-tui/releases/download/v0.1.1/hacker-news-tui-aarch64-apple-darwin.tar.gz"
-      sha256 "00100b83b7d5295efc07fd80bf8db9348d58c1e167be8309bb5a86b5d3636be2"
+      url "https://github.com/danfry1/hacker-news-tui/releases/download/v0.1.2/hacker-news-tui-aarch64-apple-darwin.tar.gz"
+      sha256 "a561dcee77ad355d0bba52903440cd0303d052bd7e97f2d75ae8306a568f118d"
 
       def install
         bin.install "hacker-news-tui"
@@ -20,16 +20,16 @@ class HackerNewsTui < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/danfry1/hacker-news-tui/releases/download/v0.1.1/hacker-news-tui-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "890238020d3a44e0592abec3c238fcdf012d0e3089a8434f4341ca87db04fe56"
+      url "https://github.com/danfry1/hacker-news-tui/releases/download/v0.1.2/hacker-news-tui-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "870a0deeddee515d2b4734f1b9ee242692ac1f80eb8fe2cf84421447da251918"
 
       def install
         bin.install "hacker-news-tui"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/danfry1/hacker-news-tui/releases/download/v0.1.1/hacker-news-tui-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "917b7fdbb0d0083059e3015f606a0d9ce65037b5c2122c04180c297227dc58bb"
+      url "https://github.com/danfry1/hacker-news-tui/releases/download/v0.1.2/hacker-news-tui-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "8ee7b599fec94dca890ec571e80468a9be1042c414b2389e29d62941c1f058b4"
 
       def install
         bin.install "hacker-news-tui"
